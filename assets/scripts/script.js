da0b0beea59c3d6fd193ae4601677ff099635c02
Guido Asbun <gdrodrigo8@gmail.com>
2021-04-17 15:10:23 -0700

    decimal point functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,6 +8,13 @@ function sendNumberValue(number) {
    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
+function addDecimal() {
+   // If no decimal, add one
+   if (!calculatorDisplay.textContent.includes('.')) {
+      calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+   }
+}
+
 // Add event listeners for numbers, operators, decimal buttons
 inputButtons.forEach((inputButton) => {
    if (inputButton.classList.length === 0) {
@@ -15,6 +22,14 @@ inputButtons.forEach((inputButton) => {
    } else if (inputButton.classList.contains('operator')) {
       inputButton.addEventListener('click', () => sendNumberValue(inputButton.value))
    }else if (inputButton.classList.contains('decimal')) {
-      inputButton.addEventListener('click', () => sendNumberValue(inputButton.value))
+      inputButton.addEventListener('click', () => addDecimal())
    }
 });
+
+// Reset display
+function resetAll() {
+   calculatorDisplay.textContent = '0';
+}
+
+// Event listener
+clearButton.addEventListener('click', resetAll);
